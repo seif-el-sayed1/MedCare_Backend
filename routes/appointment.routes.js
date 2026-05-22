a50a19a88456eefebc229c3c91d9c617c474e182
seif_elsayed <seifsayed121@gmail.com>
2026-05-23 00:11:34 +0300

    appointments routes

---
+++ routes/appointment.routes.js
@@ -0,0 +1,84 @@
+const express = require("express");
+// Classes 
+const AppointmentController = require("../controllers/appointment.controller");
+const AppointmentValidator = require("../validators/appointment.validator")
+//middlewares
+const { protect, allowedTo } = require("../middlewares/auth.middleware");
+const { USER, ADMIN, SUPER_ADMIN } = require("../utils/constants");
+// Router
+const router = express.Router();
+
+router.route("/")
+    .get(
+        protect, 
+        allowedTo(SUPER_ADMIN, ADMIN),
+        AppointmentController.getAllAppointments
+    )
+
+router.route("/:id")
+    .get(
+        protect, 
+        allowedTo(SUPER_ADMIN, ADMIN, USER),
+        AppointmentController.getOneAppointment
+    )
+    .patch(
+        protect, 
+        allowedTo(SUPER_ADMIN, ADMIN),
+        AppointmentValidator.validateUpdateAppointmentPayment,
+        AppointmentController.updateAppointmentPayment
+    )
+    .delete(
+        protect, 
+        allowedTo(SUPER_ADMIN, ADMIN),
+        AppointmentController.deleteAppointment 
+    )
+
+router.route("/:id/status")
+    .patch(
+        protect, 
+        allowedTo(SUPER_ADMIN, ADMIN),
+        AppointmentValidator.validateUpdateAppointmentStatus,
+        AppointmentController.updateAppointmentStatus
+    )
+
+router.route("/:id/consultation")
+    .patch(
+        protect, 
+        allowedTo(SUPER_ADMIN, ADMIN),
+        AppointmentController.updateAppointmentConsultation
+    )
+
+router.route("/:id/payment")
+    .post(
+        protect, 
+        allowedTo(USER),
+        AppointmentController.makePayment
+    )
+
+
+router.route("/:id/report")
+    .get(
+        protect, 
+        allowedTo(USER, ADMIN, SUPER_ADMIN),
+        AppointmentController.generateReport
+    )
+
+    
+router.route('/:id/qr').get(protect, AppointmentController.generateQRCode);
+router.route('/:id/scan').get(AppointmentController.scanAppointment);
+
+router.route("/:doctorId")
+    .post(
+        protect, 
+        allowedTo(USER),
+        AppointmentValidator.validateBookAppointment,
+        AppointmentController.bookAppointment
+    ).get(
+        protect, 
+        allowedTo(ADMIN, SUPER_ADMIN),
+        AppointmentController.getDoctorAppointments
+    )
+
+
+
+module.exports = router;
