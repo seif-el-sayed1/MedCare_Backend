af7160c2ecbe54bd385b48e3bee49443cdd76a4b
seif_elsayed <seifsayed121@gmail.com>
2026-05-25 04:00:04 +0300

    loggedin doctor routes

---
+++ routes/loggedinDoctors.routes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+// Constants
+const { DOCTOR } = require("../utils/constants");
+
+// Auth middleware
+const { protect, allowedTo } = require("../middlewares/auth.middleware");
+
+// Classes
+const LoggedInDoctorController = require("../controllers/loggedinDoctor.controller");
+const FirebaseController = require("../controllers/firebase.controller");
+// Router
+const router = express.Router();
+
+
+router.route("/me").get(protect, allowedTo(DOCTOR), LoggedInDoctorController.getDoctorProfile);
+
+router.route("/update").patch(
+    protect,
+    allowedTo(DOCTOR),
+    FirebaseController.uploadSingleImage("Doctors","profilePicture"),
+    LoggedInDoctorController.updateDoctorProfile
+);
+
+router.route("/appointments").get(protect, allowedTo(DOCTOR), LoggedInDoctorController.getMyAppointments);
+
+router.route("/:id").patch(protect, allowedTo(DOCTOR), LoggedInDoctorController.writeDiagnosis);
+
+module.exports = router;
