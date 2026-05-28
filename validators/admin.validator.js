ea673463a8400b602ed89e6eea9fdeba16209d54
seif_elsayed <seifsayed121@gmail.com>
2026-05-28 03:00:33 +0300

    add phone

--- validators/admin.validator.js
+++ validators/admin.validator.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 const asyncHandler = require("express-async-handler");
 const joiErrorHandler = require("./joiErrorHandler");
+const { phoneNumberValidator } = require("../validators/validatorComponents");
 
 class AdminValidator {
   static validateAddAdmin = asyncHandler(async (req, res, next) => {
@@ -8,6 +9,10 @@ class AdminValidator {
       firstName: Joi.string().required(),
       lastName: Joi.string().required(),
       email: Joi.string().email().required(),
+      phone: Joi.string().custom(phoneNumberValidator).required().messages({
+        "string.pattern.base": "Phone number must start with '0' and contain exactly 11 digits",
+        "any.required": "Phone number is required"
+      }),
     });
     joiErrorHandler(schema, req);
     next();
